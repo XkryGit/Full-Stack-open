cb6b397d4a5c013e54264be5381e543800c6b361
Adolfo Zambrana | Front <adolfo.zambrana@nexxyolabs.com>
2024-09-16 15:57:26 +0200

    Update part3

--- part3/backend/models/phones.js
+++ part3/backend/models/phones.js
@@ -7,8 +7,7 @@ const url = process.env.MONGODB_URI;
 console.log("connecting to", url);
 
 mongoose
-  .connect(url)
-
+  .connect(url.toString())
   .then((result) => {
     console.log("connected to MongoDB");
   })
