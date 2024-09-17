dd483fcee2db8756adb9b9dd1608f35d1feafca3
Adolfo Zambrana | Front <adolfo.zambrana@nexxyolabs.com>
2024-09-17 14:19:26 +0200

    Return

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,96 +1,80 @@
+const http = require("http");
 require("dotenv").config();
 
 const express = require("express");
-const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
-const Person = require("./models/person");
+const Phones = require("./models/phones");
 
+const app = express();
 app.use(cors());
-morgan.token("body", (request) => JSON.stringify(request.body));
-app.use(express.json());
 app.use(morgan("tiny"));
-app.use(express.static("build"));
+app.use(express.json());
+app.use(express.static("dist"));
 
-app.get("/api/persons", (request, response, next) => {
-  Person.find({})
-    .then((result) => {
-      response.json(result);
-    })
-    .catch((error) => next(error));
-});
-
-app.get("/info", (request, response, next) => {
+app.get("/info", (request, response) => {
   const date = new Date();
-  Person.find({})
-    .then((result) => {
-      response.send(
-        `<p>Phonebook has info for ${result.length} persons</p>
-             <br>
-             <p>${date}</p>`
-      );
-    })
-    .catch((error) => next(error));
+  Phones.find({}).then((Phones) => {
+    response.send(
+      `<div> <p>Phonebook has info for ${Phones.length} people</p> <p>${date}</p></div>`
+    );
+  });
 });
 
-app.get("/api/persons/:id", (request, response, next) => {
-  Person.findById(request.params.id)
-    .then((person) => {
-      if (person) {
-        response.json(person);
+app.get("/api/persons", (request, response) => {
+  Phones.find({}).then((Phone) => {
+    response.json(Phone);
+  });
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  Phones.findById(request.params.id).then((Phone) => {
+    if (Phone) {
+      response.json(Phone);
+    } else {
+      response.status(404).end();
+    }
+  });
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  Phones.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      if (result) {
+        response.status(204).end();
       } else {
-        response.status(404).end();
+        response.status(404).send({ error: "Person not found" });
       }
     })
-    .catch((error) => next(error));
+    .catch((error) => {
+      console.error(error);
+      response.status(500).send({ error: "Server error" });
+    });
 });
 
-app.delete("/api/persons/:id", (request, response, next) => {
-  Person.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end();
-    })
-    .catch((error) => next(error));
-});
-
-const postMorgan = morgan(
-  ":method :url :status :res[content-length] - :response-time ms :body"
-);
-
-app.post("/api/persons", postMorgan, (request, response, next) => {
+app.post("/api/persons", (request, response) => {
   const body = request.body;
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
 
-  if (!body.name || !body.number) {
+  if (!body.name) {
     return response.status(400).json({
-      error: "name or number missing",
+      error: "name missing",
+    });
+  } else if (!body.number) {
+    return response.status(400).json({
+      error: "number missing",
     });
   } else {
-    const person = new Person({
-      name: body.name,
-      number: body.number,
-    });
-
     person
       .save()
-      .then((person) => {
-        response.json(person);
+      .then((savedPhone) => {
+        response.json(savedPhone);
       })
-      .catch((error) => next(error));
+      .catch((error) => console.log(error));
   }
-});
-
-app.put("/api/persons/:id", (request, response, next) => {
-  const { name, number } = request.body;
-
-  Person.findByIdAndUpdate(
-    request.params.id,
-    { name, number },
-    { new: true, runValidators: true, context: "query" }
-  )
-    .then((updatedPerson) => {
-      response.json(updatedPerson);
-    })
-    .catch((error) => next(error));
 });
 
 const PORT = process.env.PORT;
